8cba499774ffd1aa1a0a9f90acc77fd3b7f3a323
Victor-Higa1997 <victor_nagahara@estudante.sesisenai.org.br>
2021-12-07 23:17:36 -0300

    Exercício Completo
    Exercício 6 atendendo o exigido, porem melhorar depois..

--- exercicio 6/app.js
+++ exercicio 6/app.js
@@ -1,23 +1,45 @@
 import { converterJsonEmEndereco } from "./Endereco.js"
+const resultadoApi = document.querySelector('pre')
 
-function consultaCep(){
+const consultaCep = () =>{
     const cep = document.querySelector('input').value
-    const resultadoApi = document.querySelector('pre')
+    
 
     const url = fetch(`https://viacep.com.br/ws/${cep}/json`)
         .then(response =>  response.json()
         )
         .then(data => {
+            console.log(data)
+           
             var resultado = converterJsonEmEndereco(data.cep, data.logradouro, data.complemento, data.bairro,
                 data.localidade, data.uf, data.ibge, data.gia, data.ddd, data.siafi)
-                //console.log(a)
+                //console.log(a) */     
 
-            for(var key in resultado){
+             for(var key in resultado){
                 var valor = resultado[key]
-                resultadoApi.innerHTML += `${key} : ${valor} </br>` 
+                
+                criaElemento(key, valor)
             }
         }
     )
+}
+
+
+const criaElemento = (chave, valor) => {
+    const pre = document.querySelector('pre')
+
+    //CRIA ELEMENTOS
+    var div = document.createElement('div')
+    var fieldset = document.createElement('fieldset')
+    var p = document.createElement('p')
+
+    //STYLE DOS ELEMENTOS
+
+    p.innerHTML =  `${chave}: ${valor}`
+
+    div.appendChild(p)
+    fieldset.appendChild(div)
+    pre.appendChild(fieldset)
 }
 
 document.getElementById('consulta').addEventListener('click', consultaCep)
